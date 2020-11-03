03b1ce56843562e80e0d2b007bea6d0f9ae0dee2
ln93 <376830464@qq.com>
2020-11-03 19:28:37 +0800

    Add files via upload

--- boot.py
+++ boot.py
@@ -1,5 +1,5 @@
 import ntptime
-
+from machine import RTC
 def sync_ntp():   
     ntptime.NTP_DELTA = 3155644800   # 可选 UTC+8偏移时间（秒），不设置就是UTC0
     ntptime.host = 'ntp1.aliyun.com'  # 可选，ntp服务器，默认是"pool.ntp.org"
@@ -20,6 +20,10 @@ def connect_and_sync():
     sta_if.active(False)
 
 
-connect_and_sync()
+rtc = RTC()
+time = rtc.datetime()
+if(time[0]<2020):
+    print('Time is outdated, Updating...')
+    connect_and_sync()
 
 
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from machine import RTC,Pin,PWM
+import machine
+import esp
 import utime
-
-def drive_motor()
+def drive_motor():
     rtc = RTC()
-    time = rtc.now()
-    if time[3] > 7 and time[3] < 17:
-        Angle = ((time[3]*60+time[4])-7*60)*150/600#10 hrs a day
-        DutyNow = 1000*((Angle-90)/180 * 2+0.5)/20
-        Pwm.duty(DutyNow)
+    time = rtc.datetime()
+    if time[4] > 7 and time[4] < 17 and time[6] > 45:
+        Angle = ((time[4]*60+time[5])-7*60)*150/600#10 hrs a day
+        DutyNow = 1000*((90-Angle)/180 * 2+1.5)/20
+        Pwm.duty(int(DutyNow))
         Pwm.init()
         utime.sleep_ms(500)
         Pwr.on()
@@ -16,23 +17,16 @@ def drive_motor()
         utime.sleep_ms(500)
         Pwm.deinit()
 
-def pulse_sleep(second):#do something every 20sec to prevent BatteryPack stop power supply
-    i=0
-    while i<second/20:
-        machine.lightsleep(20000000)#10sec
-        i=i+1
-
-
-
 Pwr=Pin(4,Pin.OUT)#power Motor
 Pwr.off()
-Pwm=PWM(Pin(5))#Motor PWM
+Pwm=PWM(Pin(12))#Motor PWM
 Pwm.freq(50)
 Pwm.duty(75)
 Pwm.deinit()
-while 1:
-    drive_motor()
-    pulse_sleep(1800)#drive motor every 0.5 hour
+drive_motor()
+esp.deepsleep(10000000)
+machine.soft_reset()
+
     
 
 
