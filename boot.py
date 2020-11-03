6315d2bdea21c81f1cd5aea687bc91b04728e4a2
ln93 <376830464@qq.com>
2020-11-03 22:00:45 +0800

    Add files via upload

--- boot.py
+++ boot.py
@@ -1,23 +1,32 @@
 import ntptime
-from machine import RTC
+from machine import RTC,Pin
+import network
+import utime
 def sync_ntp():   
     ntptime.NTP_DELTA = 3155644800   # 可选 UTC+8偏移时间（秒），不设置就是UTC0
     ntptime.host = 'ntp1.aliyun.com'  # 可选，ntp服务器，默认是"pool.ntp.org"
     ntptime.settime()   # 修改设备时间,到这就已经设置好了
 
 def connect_and_sync():
-    import network
-    sta_if = network.WLAN(network.STA_IF)
     ap_if = network.WLAN(network.AP_IF)
-    ap_if.active(False)
+    ap_if.active(True)
+    ap_if.config(essid='Req_AP_SolarHost', channel=1)
+    sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('719', '')
-        while not sta_if.isconnected():
+        sta_if.connect('SolarHost', '')
+        while not sta_if.isconnected(): 
+            p2 = Pin(2, Pin.OUT)           
+            p2.value(0)
+            utime.sleep_ms(200)
+            p2.value(1)
+            utime.sleep_ms(200)
             pass
     sync_ntp()
     sta_if.active(False)
+    ap_if.active(False)
+
 
 
 rtc = RTC()
@@ -25,5 +34,15 @@ time = rtc.datetime()
 if(time[0]<2020):
     print('Time is outdated, Updating...')
     connect_and_sync()
+else:
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(True)#it won't really show because 8266 would sleep quickly
+    ap_if.config(essid='SolarTrack_Working', channel=1)
+    utime.sleep_ms(500)
+
+    
+
+
+
 
 
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import utime
 def drive_motor():
     rtc = RTC()
     time = rtc.datetime()
-    if time[4] > 7 and time[4] < 17 and time[6] > 45:
+    if time[4] > 7 and time[4] < 17 and time[6] > 35:
         Angle = ((time[4]*60+time[5])-7*60)*150/600#10 hrs a day
         DutyNow = 1000*((90-Angle)/180 * 2+1.5)/20
         Pwm.duty(int(DutyNow))
@@ -24,7 +24,7 @@ Pwm.freq(50)
 Pwm.duty(75)
 Pwm.deinit()
 drive_motor()
-esp.deepsleep(10000000)
+esp.deepsleep(20000000)
 machine.soft_reset()
 
     
